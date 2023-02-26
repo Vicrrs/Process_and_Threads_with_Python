4cfe8d1c20c254b2077d45ebceae8789c4305ef3
Vicrrs <enricofermi2019@gmail.com>
2023-02-26 19:02:26 -0400

    compartilhando estados entre processos

--- 04_processos/estados_processos_v2.py
+++ 04_processos/estados_processos_v2.py
@@ -1,35 +1,37 @@
 import multiprocessing
 import time
+import ctypes
 
 
 def funcao(val, stat):
-    if stat == True:
-        res = val + 10
-        stat = False
+    if stat.value:
+        res = val.value + 10
+        stat.value = False
     else:
-        res = val + 20
-        val = 200
-        stat = True
+        res = val.value + 20
+        val.value = 200
+        stat.value = True
 
     print(f"O resultado da funcao 1 é {res}")
     time.sleep(0.01)
 
 
 def funcao1(val, stat):
-    if stat:
-        res = val + 30
-        stat = False
+    if stat.value:
+        res = val.value + 30
+        stat.value = False
     else:
-        res = val + 40
-        val = 400
-        stat = True
+        res = val.value + 40
+        val.value = 400
+        stat.value = True
 
     print(f"O resultado da funcao 2 é {res}")
     time.sleep(0.5)
 
+
 def main():
-    valor = 100
-    status = False
+    valor = multiprocessing.Value('i', 100)
+    status = multiprocessing.Value(ctypes.c_bool, False)
 
     p1 = multiprocessing.Process(target=funcao, args=(valor, status))
     p2 = multiprocessing.Process(target=funcao1, args=(valor, status))
